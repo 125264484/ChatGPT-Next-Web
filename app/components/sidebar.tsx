16b6acfada61d0df82eaafa77ed273cd577a4ce6
125264484 <125264484@qq.com>
2023-07-13 13:06:27 +0800

    jr commit

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,11 +117,9 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          JR ChatGPT
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-sub-title"]}>静戎 ChatGPT.</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
